23a93ca1a8a9014675f0805e6f2ea9aff075f693
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 11:50:04 +0300

    05 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let max, min;
+    if (num1 > num2) {
+        max = num1
+        min = num2
+    } else {
+        min = num1
+        max = num2
+    }  
+    if (num1<0 || num2<0 || typeof(num1) !== typeof(111)|| typeof(num2) !== typeof(111) ) {
+        return "ERROR"
+    }
+    let sum = 0;
+    for (let i = min; i<=max; i++ ) {
+        sum+=i
+    }
+    return sum
 };
 
 // Do not edit below this line
