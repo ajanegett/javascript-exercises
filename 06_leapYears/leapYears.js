bd011679b4b8cc25654111cbde495ea59c483876
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 11:58:36 +0300

    06 Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && (!(year % 100 === 0) || (year % 400 === 0))) {
+        return true
+    }
+    return false
 };
 
+// Divisible by 4, not divisible by 100, unless divisible by 400
 // Do not edit below this line
 module.exports = leapYears;
