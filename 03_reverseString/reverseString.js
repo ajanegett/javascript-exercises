29266081c059bdcdb92c1213e7cdbe1088757612
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 08:31:27 +0300

    03 Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitted = str.split("")
+    let reversed_String = ""
+    for (let i= splitted.length-1; i>=0;i--) {
+        reversed_String += splitted[i]
+    }
+    return reversed_String
 
 };
 
