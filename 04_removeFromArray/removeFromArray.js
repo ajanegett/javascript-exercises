bceb3639dcbc484444fea293eae0174887f3007c
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 11:38:11 +0300

    04 Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+    const args = removeArgs;
+    const new_Array = []
+    array.forEach(element => {
+        new_Array.push(element)
+    });
+    for (const val of array) {
+        for (const arg of args) {
+            if (val === arg) {
+                new_Array.splice(new_Array.indexOf(val), 1)
+            }
+        }
+    }
+    return new_Array
 };
 
 // Do not edit below this line
