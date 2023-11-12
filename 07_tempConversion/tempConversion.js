e21f15027670adf8f444432fe0367fa864dc3168
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 12:06:13 +0300

    07 Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  return Number(((5/9) * (value - 32)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return Number(((9/5) * (value) + 32).toFixed(1))
 };
 
 // Do not edit below this line
