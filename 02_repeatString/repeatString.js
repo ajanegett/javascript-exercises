6d86926b948d4a35de894b9528b07e835803715b
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-12 08:13:02 +0300

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    a = ''
+    if (num<0) {
+        return "ERROR"
+    }
+    for (i=1; i<=num; i++) {
+        a += word;
+    }
+    return a
 };
 
 // Do not edit below this line
