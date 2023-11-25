e6a731973ffae07364c8a2a1aa380ac074d2945f
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-25 07:42:28 +0300

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(...Numbers) {
+  let easy =Numbers.reduce((total,num) => total += num, 0)
+  return easy
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let easy =array.reduce((total,num) => total += num, 0)
+  return easy
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  for (let i = x; x > 0; x--) {
+    total *= x;
+  }
+  return total
 };
 
 // Do not edit below this line
