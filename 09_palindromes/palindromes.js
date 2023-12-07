9dd0a28e89ed88d82c22679d30d03dc2f26b2f1b
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-12-07 16:07:20 +0300

    finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const reverseString = function(str) {
+    let splitted = str.split("")
+    let reversed_String = ""
+    for (let i= splitted.length-1; i>=0;i--) {
+        reversed_String += splitted[i]
+    }
+    return reversed_String
+};
 
+
+function isLetter(x) {
+    return !(x.toLowerCase() === x.toUpperCase())
+}
+
+const palindromes = function (string) {
+    let myString = string.toLowerCase()
+    const myArray = myString.split("")
+    const lll = myArray.filter((char) => {
+        if (isLetter(char)) {
+            return true;
+        }
+        else if (char == Number(char) && char != " ") {
+            return true
+        }
+        return false
+    })
+    let newString = lll.join("")
+    return (reverseString(newString) ===  newString)
 };
 
 // Do not edit below this line
