132dd801dccd92e0fcacd12462618c10683b3718
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-12-08 12:32:29 +0300

    12 Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.sort((person1, person2) => {
+    if (person1.yearOfDeath === undefined) {
+      const d = new Date();
+      let year = d.getFullYear();
+      person1.yearOfDeath = year;
+    }
+    if (person2.yearOfDeath === undefined) {
+        const d = new Date();
+        let year = d.getFullYear();
+        person2.yearOfDeath = year;
+      }  
+    let age1 = person1.yearOfDeath - person1.yearOfBirth;
+    let age2 = person2.yearOfDeath - person2.yearOfBirth;
+    return age2 - age1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
